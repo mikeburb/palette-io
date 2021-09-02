3151dbd8d122e8c2edc53511ed21ec6c10d602ec
Mike Burbidge <84789055+mikeburb@users.noreply.github.com>
2021-09-02 12:14:54 -0700

    Initial Gemfile Generation
    Used Bundler to Generate Gemfile

---
+++ lib/palette/io.rb
@@ -0,0 +1,8 @@
+require "palette/io/version"
+
+module Palette
+  module Io
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/palette/io/version.rb
@@ -0,0 +1,5 @@
+module Palette
+  module Io
+    VERSION = "0.1.0"
+  end
+end
