64806092dccb035ddc75f15c4483ac8c305e34dc
Mike Burbidge <84789055+mikeburb@users.noreply.github.com>
2021-09-04 10:41:40 -0700

    Swatch: Hexadecimal Input
    Swatch objects can be created from hexadecimal input as a string with or without the "#". By default, the swatch stores data in the 16-bit RGB format.

--- lib/palette-io/swatch.rb
+++ lib/palette-io/swatch.rb
@@ -1,21 +1,46 @@
 require 'palette-io'
 module PaletteIO
   class Swatch
-    attr_reader :colorSpace
+    attr_reader :colorSpace, :values
 
     def initialize(*args)
       setColor(args)
     end
 
     def setColor(colorInput)
+      @values = []
       if(colorInput.length == 1)
-        colorInput = colorInput[0]
-      end
-      if(colorInput.length > 5)
-        @colorSpace = :hexadecimal
+        parseColorSingleInput(colorInput)
+      else
+
       end
     end
 
+    private
+
+    def parseColorMultiInput(colorInput)
+
+    end
+
+    def parseColorSingleInput(colorInput)
+      colorInput = colorInput[0]
+      if(colorInput.length > 5)
+        fromHexadecimal(colorInput)
+      end
+    end
+
+    def fromHexadecimal(hexcolor)
+      if(hexcolor.length > 6)
+        if(hexcolor.length == 7)
+          hexcolor.slice!(0, 1)
+        else
+          raise TypeError, "Invalid color input value."
+        end
+      end
+      hexValues = hexcolor.scan(/\w{2}/)
+      hexValues.each {|hexValue| @values << hexValue.to_i(16)}
+      @colorSpace = :rgb
+    end
 
 
   end
--- test/swatch.rb
+++ test/swatch.rb
@@ -8,11 +8,31 @@ module PaletteIO
     def setup
     end
 
-    def test_single_parameter_six_plus_digit_swatch_has_hexadecimal_color_space
-      @swatch = Swatch.new("235613")
-      @swatchTwo = Swatch.new(23,56,13)
-      assert_equal :hexadecimal, @swatch.colorSpace
-      assert @swatchTwo.colorSpace != :hexadecimal
+    #Hexadecimal
+
+    def test_hexadecimal_input_defaults_to_rgb_color_space
+      @swatch = Swatch.new("2356A3")
+      @swatchTwo = Swatch.new("#2356A3")
+      assert_equal :rgb, @swatch.colorSpace
+      assert_equal :rgb, @swatchTwo.colorSpace
     end
+
+    def test_hexadecimal_input_converts_to_rgb
+      @swatch = Swatch.new("2356A3")
+      @swatchTwo = Swatch.new("#2356A3")
+      assert_equal [35, 86, 163], @swatch.values
+      assert_equal @swatchTwo.values, @swatch.values
+    end
+
+    def test_single_value_input_longer_than_7_raises_error
+      assert_raise (TypeError) do
+        @swatch = Swatch.new("2356A323")
+      end
+    end
+
+
+
+
+
   end
 end
